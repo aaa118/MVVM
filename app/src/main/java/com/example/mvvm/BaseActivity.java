94ed54f06abdd7dc5dfcbcd355318ceb218fa2de
Adi <aaavs118@gmail.com>
2019-09-08 17:40:38 -0400

    App Crashing

---
+++ app/src/main/java/com/example/mvvm/BaseActivity.java
@@ -0,0 +1,25 @@
+package com.example.mvvm;
+
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public ProgressBar mProgressBar;
+
+    @Override
+    public void setContentView(int layoutResID) {
+        ConstraintLayout constraintLayout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_base,null);
+        FrameLayout frameLayout = constraintLayout.findViewById(R.id.activity_content);
+        getLayoutInflater().inflate(layoutResID, frameLayout, true);
+        super.setContentView(layoutResID);
+    }
+
+    public void showProgressBar(boolean visible) {
+        mProgressBar.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
+    }
+}
